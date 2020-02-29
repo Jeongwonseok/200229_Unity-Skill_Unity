855b6e0cab03ab80bcd304082cb5b6ec4c306ae3
“Jeongwonseok” <jws4281@nate.com>
2020-02-29 22:13:46 +0900

    1. Object Pooling
    - 큐를 이용한 오브젝트 풀링 스킬

---
+++ Unity-Skill/Assets/1.ObjectPooling/Script/ObjectPoolingManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+    public static ObjectPoolingManager instance;
+
+    public GameObject m_goPrefab = null;
+
+    // 객체 저장시킬 큐 생성
+    public Queue<GameObject> m_queue = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        for(int i=0; i<500; i++)
+        {
+            // 객체 생성한 뒤,  풀(큐)에 저장
+            GameObject t_object = Instantiate(m_goPrefab, Vector3.zero, Quaternion.identity);
+            m_queue.Enqueue(t_object);
+            t_object.SetActive(false);
+        }
+    }
+
+    // 사용한 객체를 풀(큐)에 반납시키는 함수
+    public void InsertQueue(GameObject p_object)
+    {
+        m_queue.Enqueue(p_object);
+        p_object.SetActive(false);
+    }
+
+    // 풀에서 객체를 빌려오는 함수
+    public GameObject GetQueue()
+    {
+        GameObject t_object = m_queue.Dequeue();
+        t_object.SetActive(true);
+        return t_object;
+    }
+}
